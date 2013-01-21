b402ec3570a8069f5e30e11aca4b29eded7d7f8b
Beutier <beutier.k@gmail.com>
2013-01-21 13:39:14 +0100

    Depot initial

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'jquery-rails'
+gem "will_paginate", :git => "https://github.com/p7r/will_paginate.git", :branch => "rails3"
+
+group :development do
+  gem 'rspec-rails', '2.5.0'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec', '2.5.0'
+  gem 'webrat', '0.7.1'
+  gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '742ba7286447cc23496be7388a0db3514a916cf63adbbb57f10f7cb7de2b73b1e72a5fa8510e9ea62145d64dca18d4722df76336547ff05b69c3816253dae70b'
